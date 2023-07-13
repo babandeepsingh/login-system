ad9d2f95eb181db837e8699661c95496796504ef
Babandeep Singh <er.babandeep@gmail.com>
2023-07-13 17:51:34 +0530

    Create page.js

---
+++ app/dashboard/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Dashboad = () => {
+  return (
+    <div>
+        <h1 className='text-2xl font-bold mb-5'>Dashboad</h1>
+        <p className='mb-5'>
+            Welcome to the dashboard
+        </p>
+    </div>
+  )
+}
+
+export default Dashboad
